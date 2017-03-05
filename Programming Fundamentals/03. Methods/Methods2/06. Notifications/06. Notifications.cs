1c963b48e40a5b033bafaaccc7dbc0bd19185571
delian1986 <delian1914@gmail.com>
2017-03-05 22:06:48 +0200

    Daily exercises

---
+++ Programming Fundamentals/03. Methods/Methods2/06. Notifications/06. Notifications.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.Notifications
+{
+    class Program
+    {
+        static void Main()
+        {
+            var operation = Console.ReadLine();
+            var message = Console.ReadLine();
+            int code = int.Parse(Console.ReadLine());
+
+            ShowSuccsess(operation, message);
+            ShowError(operation, code);
+        }
+
+        private static void ShowError(string operation, int code)
+        {
+            var reason = String.Empty;
+            Console.WriteLine($"Error: Failed to execute {operation}.\n==============================\nError Code: {code}.\nReason: {reason}.");
+            if (code>0)
+            {
+                reason= " Invalid Client Data";
+            }
+            else if (code<0)
+            {
+                reason = "Internal System Failure";
+            }
+        }
+
+        static void ShowSuccsess(string  operation, string message)
+        {
+            Console.WriteLine($"Successfully executed {operation}\n==============================\n.Message: {message}.");
+        }
+    }
+}
---
+++ Programming Fundamentals/03. Methods/Methods2/5. Integer to Base/5. Integer to Base.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Integer_to_Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = long.Parse(Console.ReadLine());
+            var neededBase = int.Parse(Console.ReadLine());
+
+            CalculateIntToBase(num,neededBase);
+        }
+
+        static void CalculateIntToBase(long num, int neededBase)
+        {
+            string res = String.Empty;
+            while (num>0)
+            {
+                var remind = num % neededBase;
+                num = num / neededBase;
+                res = remind + res;
+
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
