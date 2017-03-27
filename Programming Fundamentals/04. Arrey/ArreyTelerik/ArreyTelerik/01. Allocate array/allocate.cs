bc04bac9b3eb30b7156330f154974e090dab4f7d
delian1986 <delian1914@gmail.com>
2017-03-28 01:11:55 +0300

    First steppes in ArrayTelerik

---
+++ Programming Fundamentals/04. Arrey/ArreyTelerik/ArreyTelerik/01. Allocate array/allocate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+// Task 01: Write a program that allocates array of N integers, 
+//initializes each element by its index multiplied by 5 and 
+//the prints the obtained array on the console.
+
+
+namespace _01.Allocate_array
+{
+    class allocate
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[int.Parse(Console.ReadLine())];
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                var multiplication = i * 5;
+                Console.WriteLine(multiplication);
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/04. Arrey/ArreyTelerik/ArreyTelerik/02. Compare arrays/compare.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+
+//Task 02: Write a program that reads two integer arrays of size N
+//from the console and compares them element by element.
+
+
+
+namespace _02.Compare_arrays
+{
+    class compare
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            int []arr1=new int [n];
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                 arr1=new int[int.Parse(Console.ReadLine())];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                arr2 = new int[ int.Parse(Console.ReadLine()) ];
+            }
+            if (arr1.SequenceEqual(arr2))
+            {
+                string res = "Equal";
+                Console.WriteLine(res);
+            }
+            else 
+            {
+                string res = "Not equal";
+                Console.WriteLine(res);
+            }
+
+
+
+        }
+    }
+}
