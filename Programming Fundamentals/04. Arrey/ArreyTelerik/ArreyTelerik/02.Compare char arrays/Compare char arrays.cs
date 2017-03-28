7069ac574f40d07716bb9ef69d6b8928c552b55f
delian1986 <delian1914@gmail.com>
2017-03-28 16:39:08 +0300

    my little progress in arrays... significant progress inc soonTM

---
+++ Programming Fundamentals/04. Arrey/ArreyTelerik/ArreyTelerik/02.Compare char arrays/Compare char arrays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Task 03:
+//Write a program that compares two char arrays 
+//lexicographically (letter by letter).
+
+
+namespace _02.Compare_char_arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            string[] first = new string[] { input };
+            string[] second = new string[] { input2 };
+
+            var res = String.Empty;
+
+            if (first.Length==second.Length)
+            {
+                res = "=";
+            }
+            if (first.Length>second.Length)
+            {
+                res = ">";
+            }
+            if (first.Length<second.Length)
+            {
+                res = "<";
+
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ Programming Fundamentals/04. Arrey/ArreyTelerik/ArreyTelerik/CompareArray/02.CompareArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+//Task 02:
+//Write a program that reads two integer arrays of size N 
+//from the console and compares them element by element.
+
+namespace CompareArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr1= new int[n];
+            int[] arr2 = new int[n] ;
+
+            for (int i = 0; i <arr1.Length; i++)
+            {
+                arr1[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = int.Parse(Console.ReadLine());
+            }
+
+            if (arr1.SequenceEqual(arr2))
+            {
+                var res = "Equal";
+                Console.WriteLine(res);
+
+            }
+            else
+            {
+                var res = "Not equal";
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
