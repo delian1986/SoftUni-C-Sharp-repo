6ac0990634bd214203ef276c3ffeaca39d7c3a9e
delian1986 <delian1914@gmail.com>
2017-03-28 09:17:53 +0300

    Methods homework solutions Telerik

---
+++ Programming Fundamentals/03. Methods/MethodsTelerikHomework/Solution1/01.SayHello/hello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace _01.SayHello
+{
+    class hello
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+
+            PrintHelloName(name);
+        }
+
+        static void PrintHelloName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ Programming Fundamentals/03. Methods/MethodsTelerikHomework/Solution1/02. Get largest number/GetLargest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _02.Get_largest_number
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            GetMax(num1, num2, num3);
+
+
+        }
+
+        private static void GetMax(int num1, int num2, int num3)
+        {
+            var bigger = Math.Max(num1, num2);
+            var largest = Math.Max(bigger, num3);
+            Console.WriteLine(largest);
+        }
+    }
+}
