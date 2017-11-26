bd56ac27777573fc6bd140a34948abf084be39fe
delian1986 <delian1914@gmail.com>
2017-11-26 19:10:22 +0200

    Programming Fundamentals

---
+++ Fundamentals of Programming/01. Hello Wold lekcia/01. Hello Word Lekcia/11. Real Number Types/11. Real Number Types.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Real_Number_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte n = sbyte.Parse(Console.ReadLine());
+            decimal num = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Fundamentals of Programming/01. Hello Wold lekcia/01. Hello Word Lekcia/13. Hornet Wings/13. Hornet Wings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Hornet_Wings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            double dist = (n / 1000) * m;
+            var flap = n / 100;
+            var sec = (n / p) * 5;
+            var res = sec + flap;
+            Console.WriteLine($"{dist:f2} m.");
+            Console.WriteLine($"{res} s.");
+
+        }
+    }
+}
