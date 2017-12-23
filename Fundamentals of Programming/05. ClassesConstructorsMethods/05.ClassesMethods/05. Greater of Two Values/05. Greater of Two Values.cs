8ecb4b70d83c646e0de48a84084e5a0628d72be6
delian1986 <delian1914@gmail.com>
2017-12-23 23:30:48 +0200

    method

--- Fundamentals of Programming/05. ClassesConstructorsMethods/05.ClassesMethods/05. Greater of Two Values/05. Greater of Two Values.cs
+++ Fundamentals of Programming/05. ClassesConstructorsMethods/05.ClassesMethods/05. Greater of Two Values/05. Greater of Two Values.cs
@@ -10,7 +10,27 @@ namespace _05.Greater_of_Two_Values
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+
+            TypeComparer(input);
+        }
+
+        private static void TypeComparer(string input)
+        {
+            if (input == "int")
+            {
+                int val1 = int.Parse(Console.ReadLine());
+                int val2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Max(val1, val2));
+            }
+            else if (input == "char")
+            {
+                char val1 = char.Parse(Console.ReadLine());
+                char val2 = char.Parse(Console.ReadLine());
+                Console.WriteLine(val1.CompareTo(val2));
+
+            }
         }
     }
 }
