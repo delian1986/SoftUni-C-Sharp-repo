e8e377ef5ed4510cc15f40dbac07cd38fd4b5346
delian1986 <delian1914@gmail.com>
2017-12-28 01:18:17 +0200

    homework maps

--- Fundamentals of Programming/04. Maps/04. Maps/11. Legendary Farming/11. Legendary Farming.cs
+++ Fundamentals of Programming/04. Maps/04. Maps/11. Legendary Farming/11. Legendary Farming.cs
@@ -10,6 +10,96 @@ namespace _11.Legendary_Farming
     {
         static void Main(string[] args)
         {
+            SortedDictionary<string,  int> legendary = new SortedDictionary<string,int>();
+
+
+            legendary.Add("shards", 0);
+            legendary.Add("fragments", 0);
+            legendary.Add("motes", 0);
+
+            SortedDictionary<string, int> junk = new SortedDictionary<string, int>();
+            string obtained = String.Empty;
+            bool legend = false;
+
+            while (!legend)
+            {
+                string[] drop = Console.ReadLine().ToLower().Split().ToArray();
+
+                for (int i = 1; i < drop.Length; i += 2)
+                {
+                    int quantity = int.Parse(drop[i - 1]);
+                    if (legendary.ContainsKey(drop[i]))
+                    {
+                        legendary[drop[i]] += quantity;
+                        if (legendary["shards"]>=250)
+                        {
+                            legendary["shards"] -= 250;
+                            obtained = "Shadowmourne obtained!";
+                            legend = true;
+                            break;
+                        }
+                        else if (legendary["fragments"] >= 250)
+                        {
+                            legendary["fragments"] -= 250;
+                            obtained = "Valanyr obtained!";
+                            legend = true;
+                            break;
+
+                        }
+                        else if (legendary["motes"]>=250)
+                        {
+                            legendary["motes"] -= 250;
+                            obtained = "Dragonwrath obtained!";
+                            legend = true;
+                            break;
+                        }
+                    }
+                    //else if (legendary.ContainsKey(drop[i]))
+                    //{
+                    //    legendary["fragments"] += quantity;
+                    //    if (l)
+                    //    {
+                    //        
+                    //    }
+                    //}
+                    //else if ()
+                    //{
+                    //    legendary["motes"] += quantity;
+                    //    if (legendary["motes"] >= 250)
+                    //    {
+                    //        
+                    //
+                    //    }
+                    //}
+
+                    else
+                    {
+                        if (!junk.ContainsKey(drop[i]))
+                        {
+                            junk.Add(drop[i], quantity);
+                        }
+                        else
+                        {
+                            junk[drop[i]] += quantity;
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(obtained);
+
+            foreach (var item in legendary.OrderByDescending(n =>n.Value))
+            {
+                
+                    Console.WriteLine("{0}: {1}",string.Join("",item.Key),string.Join("",item.Value));
+                
+            }
+
+            foreach (var item in junk.OrderBy(n=>n.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            
         }
     }
 }
