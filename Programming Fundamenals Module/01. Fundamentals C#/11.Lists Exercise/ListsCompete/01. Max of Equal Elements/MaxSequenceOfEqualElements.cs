1eb4ec747f8e92c9a60cb1ea677e9b326ebcf83f
delian1986 <delian1914@gmail.com>
2018-02-09 12:00:19 +0200

    MaxSequenceOfEqualElements solution

---
+++ Programming Fundamenals Module/01. Fundamentals C#/11.Lists Exercise/ListsCompete/01. Max of Equal Elements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Max_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList(); 
+
+            var mostFrequent=nums.GroupBy(x=>x);
+            
+
+            foreach (var num in mostFrequent.OrderByDescending(x=>x.Count()))
+            {
+                var element=num.Key;
+                var ocurr=num.Count();
+                Console.WriteLine(string.Concat(Enumerable.Repeat(element+" ",ocurr)));
+                break;
+            }
+        }
+    }
+}
