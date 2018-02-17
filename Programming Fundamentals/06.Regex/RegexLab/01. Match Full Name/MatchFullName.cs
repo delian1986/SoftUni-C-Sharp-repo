c6c6ec8d21afa10ab4100b05851e0c56217ae24d
delian1986 <delian1914@gmail.com>
2018-02-17 23:17:14 +0200

    First steppes in regex

---
+++ Programming Fundamentals/06.Regex/RegexLab/01. Match Full Name/MatchFullName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// This program match two words starting with capital letter.
+/// </summary>
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames=Regex.Matches(names,pattern);
+
+            foreach (Match matchedName in matchedNames)
+            {
+                Console.WriteLine(matchedName.Value+" ");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/06.Regex/RegexLab/02. Match Phone Number/MatchPhoneNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Match_Phone_Number
+{
+    class MatchPhoneNumber
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
