50df7150388ab1fcbc2f0afdd05bee0b6a8b4dac
delian1986 <delian1914@gmail.com>
2018-02-26 19:51:39 +0200

    Dictionaries and List Exercises

---
+++ Programming Fundamenals January 2018/07.Dictionaries, Lambda Expressions and LINQ/Dictionariesa And Lists/07. Take Skip Rope/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// 80/100
+/// https://judge.softuni.bg/Contests/Practice/Index/582#6
+/// </summary>
+
+namespace _07._Take_Skip_Rope
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input= Console.ReadLine();
+
+            string patternForLetters=@"[A-Za-z_]+";
+            string patternForNums=@"\d+";
+
+            string nums=string.Empty;
+            string letters=string.Empty;
+
+            List<string>letterList= new List<string>();
+
+            foreach (Match num in Regex.Matches(input,patternForNums))
+            {
+                nums+=num;
+            }
+
+            foreach (Match letter in Regex.Matches(input,patternForLetters))
+            {
+                letters+=letter;
+            }
+
+            List<int> takeList=new List<int>();
+            List<int> skipList=new List<int>();
+
+            for (int i = 0; i < nums.ToCharArray().Length; i++)
+            {
+                if (i%2==0)
+                {
+                    takeList.Add(nums[i]);
+                }
+                else
+                {
+                    skipList.Add(nums[i]);
+                }
+            }
+
+            int totalSkip = 0;
+            string finalMessage = String.Empty;
+ 
+            for (int i = 0; i < takeList.Count; i++)
+            {
+                int takeCount = takeList[i];
+                int skipCount = skipList[i];
+ 
+                finalMessage += new string(
+                    letters.Skip(totalSkip).
+                    Take(takeCount).ToArray());
+ 
+                totalSkip += takeCount + skipCount;
+            }
+ 
+            Console.WriteLine(finalMessage);
+
+        }
+    }
+}
