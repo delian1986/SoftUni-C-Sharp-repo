b1bdf583dd095fae667a791b82aaac5e86ffe7e0
delian1986 <delian1914@gmail.com>
2018-02-28 23:03:50 +0200

    Regex Exercise

---
+++ Programming Fundamenals January 2018/09. StringAndTextProceeding/StringsAndRegEx/05. Only Letters/OnlyLetters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05._Only_Letters
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"([\d]+)([A-Za-z])";
+            Regex reg= new Regex(pattern);
+
+            MatchCollection replace= reg.Matches(text);
+
+            foreach (Match item in replace)
+            {
+                text= Regex.Replace(text,item.Groups[1].Value.ToString(),item.Groups[2].Value.ToString());
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
