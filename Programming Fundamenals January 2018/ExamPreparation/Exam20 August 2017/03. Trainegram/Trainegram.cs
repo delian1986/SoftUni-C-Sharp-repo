c9a8bb00a71aee7041c66f82c86562a50979ebbf
delian1986 <delian1914@gmail.com>
2018-03-01 22:14:08 +0200

    Train Exam August 2017

---
+++ Programming Fundamenals January 2018/ExamPreparation/Exam20 August 2017/03. Trainegram/Trainegram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _03._Trainegram
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input=string.Empty;
+            List<string>trains=new List<string>();
+
+            while ((input=Console.ReadLine())!="Traincode!")
+            {
+                Regex train= new Regex(@"^(\<\[[^a-zA-Z0-9]+\]\.)(.\[[A-Za-z0-9]+\].)*$");
+
+                if (train.IsMatch(input))
+                {
+                    trains.Add(train.ToString());
+                } 
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine,trains));
+        }
+    }
+}
