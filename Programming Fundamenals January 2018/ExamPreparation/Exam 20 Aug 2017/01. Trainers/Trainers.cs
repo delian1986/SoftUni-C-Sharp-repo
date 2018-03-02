134d179db03e08eefaf927965877b10c6589e33d
delian1986 <delian1914@gmail.com>
2018-03-02 13:24:58 +0200

    Trainers from exam 20 August 2017

---
+++ Programming Fundamenals January 2018/ExamPreparation/Exam 20 Aug 2017/01. Trainers/Trainers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// https://judge.softuni.bg/Contests/Practice/Index/741#0
+/// 100/100
+/// </summary>
+
+namespace _01._Trainers
+{
+    class Trainers
+    {
+        static void Main()
+        {
+            long num = long.Parse(Console.ReadLine());
+            Dictionary<string, decimal> bestTrainers = new Dictionary<string, decimal>()
+            {
+                {"Technical",0},
+                {"Practical",0 },
+                {"Theoretical",0}
+            };
+
+            for (long i = 0; i < num; i++)
+            {
+                long distance = long.Parse(Console.ReadLine());
+                decimal cargo = decimal.Parse(Console.ReadLine());
+                string teamName = Console.ReadLine();
+
+                decimal cargoInKg = cargo * 1000m;
+                decimal distInMeters = distance * 1600m;
+                decimal fuelCons = distInMeters * 0.7m;
+                decimal fuelPrice = fuelCons * 2.5m;
+                decimal cargoIncome = cargoInKg * 1.5m;
+                decimal result = cargoIncome - fuelPrice;
+
+                bestTrainers[teamName] += result;
+
+            }
+
+            
+            var best=bestTrainers.OrderByDescending(x=>x.Value).First();
+
+             Console.WriteLine($"The {best.Key} Trainers win with ${best.Value:f3}.");
+        }
+    }
+}
