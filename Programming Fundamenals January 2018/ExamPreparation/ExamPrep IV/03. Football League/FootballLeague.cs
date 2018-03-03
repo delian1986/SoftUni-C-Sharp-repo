229e7be2221d39a46b31489b34130aeb6f00c952
delian1986 <delian1914@gmail.com>
2018-03-03 22:05:39 +0200

    Sample Exam

---
+++ Programming Fundamenals January 2018/ExamPreparation/ExamPrep IV/03. Football League/FootballLeague.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// 100/100
+/// https://judge.softuni.bg/Contests/Practice/Index/456#2
+/// </summary>
+
+namespace _03._Football_League
+{
+    class Team
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public long Goals { get; set; }
+    }
+    class FootballLeague
+    {
+        static void Main()
+        {
+            List<Team> standing = new List<Team>();
+            string pattern=Regex.Escape(Console.ReadLine());
+
+            string patternTeams = $@"({pattern})(.*?)(\1)";
+            string scorePattern = @"(\d+):(\d+)";
+
+            Regex reg = new Regex(patternTeams);
+            string text = string.Empty;
+
+            while ((text = Console.ReadLine()) != "final")
+            {
+                var teamMatches = Regex.Matches(text, patternTeams);
+                int teamCounter = 1;
+                Team curr1 = new Team();
+                Team curr2 = new Team();
+
+
+                foreach (Match item in teamMatches)
+                {
+                    string team = item.Groups[2].Value.ToUpper();
+                    team = NameReverse(team);
+                    if (teamCounter == 1)
+                    {
+                        curr1.Name = team;
+
+                        var exist = standing.FirstOrDefault(x => x.Name == team);
+                        if (exist == null)
+                        {
+                            standing.Add(curr1);
+                        }
+                        else
+                        {
+                            curr1 = exist;
+                        }
+                    }
+                    else
+                    {
+                        curr2.Name = team;
+                        var exist = standing.FirstOrDefault(x => x.Name == team);
+                        if (exist == null)
+                        {
+                            standing.Add(curr2);
+                        }
+                        else
+                        {
+                            curr2 = exist;
+                        }
+                    }
+                    teamCounter++;
+                }
+
+                var resultMatches = Regex.Matches(text, scorePattern);
+
+                foreach (Match score in resultMatches)
+                {
+                    long goals1 = long.Parse(score.Groups[1].Value);
+                    long goals2 = long.Parse(score.Groups[2].Value);
+
+                    curr1.Goals += goals1;
+                    curr2.Goals += goals2;
+
+                    if (goals1 == goals2)
+                    {
+                        curr1.Points += 1;
+                        curr2.Points += 1;
+                    }
+                    else if (goals1 > goals2)
+                    {
+                        curr1.Points += 3;
+                    }
+                    else if (goals1 < goals2)
+                    {
+                        curr2.Points += 3;
+                    }
+                }
+            }
+            var orderByPoints = standing
+                .OrderByDescending(x => x.Points)
+                .ThenBy(x=>x.Name);
+
+            int count = 1;
+            Console.WriteLine("League standings:");
+            foreach (var team in orderByPoints)
+            {
+                Console.WriteLine($"{count}. {team.Name} {team.Points}");
+                count++;
+            }
+
+            var orderByGoals=standing
+                .OrderByDescending(x=>x.Goals)
+                .ThenBy(x=>x.Name);
+            count=1;
+
+            Console.WriteLine($"Top 3 scored goals:");
+            foreach (var topScore in orderByGoals)
+            {
+                Console.WriteLine($"- {topScore.Name} -> {topScore.Goals}");
+                count++;
+                if (count>3)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static string NameReverse(string team)
+        {
+            char[] revesing = team.ToCharArray();
+            Array.Reverse(revesing);
+            return new string(revesing);
+        }
+    }
+}
---
+++ Programming Fundamenals January 2018/ExamPreparation/SampleExam III/02. Command Interpreter/CommandInterpreter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// 100/100
+/// https://judge.softuni.bg/Contests/Practice/Index/455#1
+/// </summary>
+
+namespace _02._Command_Interpreter
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] sequence = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] args = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = args[0];
+
+                if (command == "reverse")
+                {
+                    int start = int.Parse(args[2]);
+                    int count = int.Parse(args.Last());
+                    bool isValid = ValidatingIndexes(start, count, sequence.Length);
+                    if (!isValid)
+                    {
+                        continue;
+                    }
+
+                    sequence = Reverse(start, count, sequence);
+                }
+                else if (command == "sort")
+                {
+                    int start = int.Parse(args[2]);
+                    int count = int.Parse(args.Last());
+                    bool isValid = ValidatingIndexes(start, count, sequence.Length);
+                    if (!isValid)
+                    {
+                        continue;
+                    }
+
+                    sequence = Sort(start, count, sequence);
+
+                }
+                else if (command == "rollRight")
+                {
+                    int countTimes = int.Parse(args[1]);
+                    bool isValid=ValidatingRollCount(countTimes);
+                    if (!isValid)
+                    {
+                        continue;
+                    }
+                    sequence = RollRight(countTimes, sequence);
+                }
+                else if (command == "rollLeft")
+                {
+                    int countTimes = int.Parse(args[1]);
+                     bool isValid=ValidatingRollCount(countTimes);
+                    if (!isValid)
+                    {
+                        continue;
+                    }
+                    sequence=RollLeft(countTimes,sequence);
+                }
+            }
+
+            Console.WriteLine("[" + string.Join(", ", sequence) + "]");
+        }
+
+        private static bool ValidatingRollCount(int countTimes)
+        {
+            if (countTimes<0)
+            {
+                Console.WriteLine("Invalid input parameters.");
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static string[] RollLeft(int countTimes, string[] sequence)
+        {
+            //1 2 3 4 5 <-
+            countTimes%=sequence.Length;
+            string[]firstPart=sequence.Take(countTimes).ToArray();
+            string[]secondPart=sequence.Skip(countTimes).ToArray();
+            return secondPart.Concat(firstPart).ToArray();
+        }
+
+        private static string[] RollRight(int countTimes, string[] sequence)
+        {
+            //1 2 3 4 5 ->
+            countTimes%=sequence.Length;
+            string[]firstPart=sequence.Take(sequence.Length-countTimes).ToArray();
+            string[]secondPart=sequence.Skip(sequence.Length-countTimes).ToArray();
+            return secondPart.Concat(firstPart).ToArray();
+        }
+
+        private static string[] Sort(int start, int count, string[] sequence)
+        {
+            string[]firstPart=sequence.Take(start).ToArray();
+            string[]sortedPart=sequence.Skip(start).Take(count).OrderBy(x=>x).ToArray();
+            string[]lastPart=sequence.Skip(start+count).ToArray();
+            return firstPart.Concat(sortedPart).Concat(lastPart).ToArray();
+        }
+
+        private static string[] Reverse(int start, int count, string[] sequence)
+        {
+            string[]firstPart=sequence.Take(start).ToArray();
+            string[]reversedPart=sequence.Skip(start).Take(count).Reverse().ToArray();
+            string[]lastPart=sequence.Skip(start+count).ToArray();
+            return firstPart.Concat(reversedPart).Concat(lastPart).ToArray();
+        }
+
+        private static bool ValidatingIndexes(int start, int count, int seqCount)
+        {
+            bool isValid = true;
+
+            if (start < 0 || start >= seqCount || start + count < 0 || start + count-1 >= seqCount)
+            {
+                Console.WriteLine("Invalid input parameters.");
+                return isValid = false;
+            }
+            else
+            {
+                return isValid;
+            }
+        }
+    }
+}
