5afdafbd8bc9a08f67a4e1bcbffc600fb5255252
delian1986 <delian1914@gmail.com>
2018-03-13 00:41:43 +0200

    Jagged Array Lab Completed

---
+++ Old Courses/C# Advanced Old/02. Jagged Arrays/3. Group Numbers/GroupNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Group_Numbers
+{
+    class GroupNumbers
+    {
+        static void Main()
+        {
+            int[]sequence=Console.ReadLine().Split(new[]{", " },StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[]zero=sequence.Where(x=>Math.Abs(x)%3==0).ToArray();
+            int[]one=sequence.Where(x=>Math.Abs(x)%3==1).ToArray();
+            int[]two=sequence.Where(x=>Math.Abs(x)%3==2).ToArray();
+
+            Console.WriteLine(string.Join(" ",zero));
+            Console.WriteLine(string.Join(" ",one));
+            Console.WriteLine(string.Join(" ",two));
+
+        }
+    }
+}
---
+++ Old Courses/C# Advanced Old/02. Jagged Arrays/4. Pascal Triangle/PascalTriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4._Pascal_Triangle
+{
+    class PascalTriangle
+    {
+        static void Main()
+        {
+            int heigth=int.Parse(Console.ReadLine());
+            long[][]triangle=new long[heigth][];
+
+            for (int row = 0; row < heigth; row++)
+            {
+                triangle[row]=new long[row+1];
+                triangle[row][0]=1;
+                triangle[row][triangle[row].Length-1]=1;
+
+                for (int col = 1; col < triangle[row].Length-1; col++)
+                {
+                    triangle[row][col]=triangle[row-1][col-1]+
+                                        triangle[row-1][col];
+
+                }
+            }
+
+            foreach (var row in triangle)
+            {
+                Console.WriteLine(string.Join(" ",row));
+            }
+
+        }
+    }
+}
