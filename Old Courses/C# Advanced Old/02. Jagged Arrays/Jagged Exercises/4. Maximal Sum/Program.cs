6ae92d3d286bf007f97df3ea528fcca685c2d2d1
delian1986 <delian1914@gmail.com>
2018-03-24 19:08:31 +0200

    Maximal Sum solved

--- Old Courses/C# Advanced Old/02. Jagged Arrays/Jagged Exercises/4. Maximal Sum/Program.cs
+++ Old Courses/C# Advanced Old/02. Jagged Arrays/Jagged Exercises/4. Maximal Sum/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Linq;
 
+/// <summary>
+/// 100/100
+/// https://judge.softuni.bg/Contests/Practice/Index/573#3
+/// Find the maximal sum of 3x3 matrix in matrix.
+/// </summary>
+
 namespace _4._Maximal_Sum
 {
     class Program
     {
         static void Main()
         {
+
             int[] args = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
@@ -14,6 +21,8 @@ namespace _4._Maximal_Sum
             int rows = args[0];
             int cols = args[1];
             int[][] matrix = new int[rows][];
+            string bestArgs=string.Empty;
+
             for (int row = 0; row < matrix.Length; row++)
             {
                 matrix[row] = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
@@ -34,18 +43,13 @@ namespace _4._Maximal_Sum
                     if (maxThreeByThreeSum<threeBythreeSum)
                     {
                         maxThreeByThreeSum=threeBythreeSum;
-                        for (int bestRow = 0; bestRow < best.Length; bestRow++)
-                        {
-                            for (int bestCol = 0; bestCol < 3; bestCol++)
-                            {
-                                best[bestRow]=matrix[row][col];
-                            }
-                        }
+                        bestArgs=(matrix[row][col]+ " "+matrix[row][col+1]+ " "+matrix[row][col+2]+ Environment.NewLine+matrix[row+1][col]+" "+matrix[row+1][col+1]+" "+matrix[row+1][col+2]+Environment.NewLine+matrix[row+2][col]+" "+matrix[row+2][col+1]+" "+matrix[row+2][col+2]).ToString();
                     }
                 }
             }
 
-            Console.WriteLine(maxThreeByThreeSum);
+            Console.WriteLine($"Sum = {maxThreeByThreeSum}");
+            Console.WriteLine(bestArgs);
         }
     }
 }
