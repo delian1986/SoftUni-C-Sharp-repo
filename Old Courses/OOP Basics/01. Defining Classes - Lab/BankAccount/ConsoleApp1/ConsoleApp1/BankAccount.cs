59e5dc208bf289da3db038b6c637018ac5290737
delian1986 <delian1914@gmail.com>
2018-08-17 12:11:37 +0300

    Bank Account Lab Defining Classes solved

---
+++ Old Courses/OOP Basics/01. Defining Classes - Lab/BankAccount/ConsoleApp1/ConsoleApp1/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class BankAccount
+{
+    private int id;
+    private decimal balance;
+
+    public int ID
+    {
+        get
+        {
+            return this.id;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                this.id = value;
+            }
+        }
+    }
+
+    public decimal Balance
+    {
+        get
+        {
+            return this.balance;
+        }
+        set
+        {
+            this.balance = value;
+        }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (this.balance - amount>=0)
+        {
+            this.Balance -= amount;
+
+        }
+        else
+        {
+            throw new ArgumentException("You cannot withdraw more than you have in the balance. Add more $$$");
+
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{this.ID}, balance {this.Balance:f2}";
+    }
+
+
+}
---
+++ Old Courses/OOP Basics/01. Defining Classes - Lab/BankAccount/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+    //    Dictionary<int, BankAccount> accounts = new Dictionary<int, BankAccount>();
+
+    //    string input;
+
+    //    while ((input = Console.ReadLine()) != "End")
+    //    {
+    //        var commands = input.Split();
+    //        string action = commands[0];
+
+    //        switch (action)
+    //        {
+    //            case "Create":
+    //                Create(commands, accounts);
+    //                break;
+    //            case "Deposit":
+    //                Deposit(commands, accounts);
+    //                break;
+    //            case "Withdraw":
+    //                Withdraw(commands, accounts);
+    //                break;
+    //            case "Print":
+    //                Print(commands, accounts);
+    //                break;
+                
+    //        }
+
+    //    }
+    //}
+
+    //private static void Print(string[] commands, Dictionary<int, BankAccount> accounts)
+    //{
+    //    int id = int.Parse(commands[1]);
+    //    if (accounts.ContainsKey(id))
+    //    {
+    //        Console.WriteLine(accounts[id].ToString()); 
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Account does not exist");
+    //    }
+    //}
+
+    //private static void Withdraw(string[] commands, Dictionary<int, BankAccount> accounts)
+    //{
+    //    var acc = new BankAccount();
+    //    decimal amount = decimal.Parse(commands[2]);
+    //    int id = int.Parse(commands[1]);
+
+    //    if (accounts.ContainsKey(id))
+    //    {
+    //        if (accounts[id].Balance >= amount)
+    //        {
+    //            accounts[id].Balance -= amount;
+
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Insufficient balance");
+    //        }
+    //    }
+    //    else
+    //    {
+
+    //        Console.WriteLine("Account does not exist");
+    //    }
+
+    //}
+
+    //private static void Deposit(string[] commands, Dictionary<int, BankAccount> accounts)
+    //{
+    //    var acc = new BankAccount();
+    //    int id = int.Parse(commands[1]);
+    //    decimal amount = decimal.Parse(commands[2]);
+
+    //    if (accounts.ContainsKey(id))
+    //    {
+    //        accounts[id].Balance += amount;
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Account does not exist");
+    //    }
+    //}
+
+    //private static void Create(string[] commands, Dictionary<int, BankAccount> accounts)
+    //{
+    //    int id = int.Parse(commands[1]);
+    //    if (accounts.ContainsKey(id))
+    //    {
+    //        Console.WriteLine("Account already exists");
+    //    }
+    //    else
+    //    {
+    //        var acc = new BankAccount();
+    //        acc.ID = id;
+    //        accounts.Add(id, acc);
+
+    //    }
+    }
+}
+
