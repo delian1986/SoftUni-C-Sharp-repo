9126ffa0b8dd0d64c2c9ab872f2f3b20c2fb264d
delian1986 <delian1914@gmail.com>
2018-08-29 23:46:59 +0300

    Matrix exercises

---
+++ Old Courses/C# Advanced Old/02. Jagged Arrays/Jagged Exercises/0. PlayGround/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _0._PlayGround
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[][] matrix = new int[4][];
+
+            //fill matrix
+            int number = 0;
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = new int[4];
+                for (int col = 0; col < 4; col++)
+                {
+                    matrix[row][col] = ++number;
+                }
+            }
+
+            int 
+            //swap rows
+            //swap row 0 with row 3
+            int[] tempArrayRowZero = new int[4];
+            int[] tempArrayRowThree = new int[4];
+            int[][] swapedRowsMatrix = new int[4][];
+            tempArrayRowZero = matrix[0];
+            tempArrayRowThree = matrix[3];
+            swapedRowsMatrix[0] = tempArrayRowThree;
+            swapedRowsMatrix[3] = tempArrayRowZero;
+
+            //swap columns
+            //swap col 0 with col 2
+            int[][] swapedColsMatrix = matrix.ToArray();
+
+            int[] tempArrColZero = new int[4];
+            int[] tempArrColTwo = new int[4];
+            int i = 0;
+            for (int row = 0; row < matrix.Length; row++, i++)
+            {
+                tempArrColZero[i] = matrix[row][0];
+                tempArrColTwo[i] = matrix[row][2];
+            }
+            i = 0;
+            for (int row = 0; row < matrix.Length; row++,i++)
+            {
+                swapedColsMatrix[row][0] = tempArrColTwo[i];
+                swapedColsMatrix[row][2] = tempArrColZero[i];
+            }
+
+        }
+    }
+}
---
+++ Old Courses/C# Advanced Old/02. Jagged Arrays/Jagged Exercises/5. Rubik Matrix/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// 0/100
+/// </summary>
+
+namespace _5._Rubik_Matrix
+{
+    class Program
+    {
+        private static int[,] matrix;
+        private static int rows;
+        private static int cols;
+
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            rows = dimensions[0];
+            cols = dimensions[1];
+            int number = 1;
+            matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = number++;
+                }
+            }
+
+            int numOfCommands = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int index = int.Parse(input[0]);
+                string direction = input[1];
+                int rotations = int.Parse(input[2]);
+
+                switch (direction)
+                {
+                    case "left":
+                        MoveRow(index, cols - rotations);
+                        break;
+
+                    case "right":
+                        MoveRow(index, rotations);
+                        break;
+
+                    case "up":
+                        MoveColumn(index, rows - rotations);
+                        break;
+
+                    case "down":
+                        MoveColumn(index, rotations);
+                        break;
+                }
+            }
+
+            RearrageMatrix();
+
+        }
+
+        private static void RearrageMatrix()
+        {
+
+        }
+
+        private static void MoveRow(int index, int rotations)
+        {
+            rotations %= cols;
+            int[] tempArr = new int[cols];
+
+            for (int col = 0; col < tempArr.Length; col++)
+            {
+                int replacement = col + rotations;
+
+                replacement %= cols;
+                tempArr[replacement] = matrix[index, col];
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[index, col] = tempArr[col];
+            }
+        }
+
+        private static void MoveColumn(int index, int rotations)
+        {
+            rotations %= rows;
+            int[] tempArr = new int[rows];
+
+            for (int row = 0; row < tempArr.Length; row++)
+            {
+                int replacement = row + rotations;
+
+                replacement %= rows;
+                tempArr[replacement] = matrix[index, row];
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row, index] = tempArr[row];
+            }
+        }
+    }
+}
