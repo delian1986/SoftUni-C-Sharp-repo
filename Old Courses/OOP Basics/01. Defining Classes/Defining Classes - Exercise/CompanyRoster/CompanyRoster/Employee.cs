55054c5a37436c56ada46ad3520c85ff58525acc
delian1986 <delian1914@gmail.com>
2018-08-30 12:51:15 +0300

    Company Rooster exercise solved

--- Old Courses/OOP Basics/01. Defining Classes/Defining Classes - Exercise/CompanyRoster/CompanyRoster/Employee.cs
+++ Old Courses/OOP Basics/01. Defining Classes/Defining Classes - Exercise/CompanyRoster/CompanyRoster/Employee.cs
@@ -2,9 +2,49 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CompanyRoster
+public class Employee
 {
-    class Employee
+    private string name;
+    private decimal salary;
+    private string position;
+    private string departement;
+    private string email;
+    private int age;
+
+    public Employee(string name, decimal salary, string position, string departement)
     {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.departement = departement;
+        this.age = -1;
+        this.email = "n/a";
     }
+
+    public string Email
+    {
+        get
+        {
+            return this.email;
+        }
+        set
+        {
+            this.email = value;
+        }
+    }
+
+    public int Age { get { return this.age; } set { this.age = value; } }
+
+    public string Department { get { return this.departement; } }
+
+    public decimal Salary { get { return this.salary; } }
+
+    public string PrintEmployeeInfo()
+    {
+        return $"{this.name} {this.Salary:f2} {this.Email} {this.Age}";
+    }
+
+
+
 }
+
--- Old Courses/OOP Basics/01. Defining Classes/Defining Classes - Exercise/CompanyRoster/CompanyRoster/Program.cs
+++ Old Courses/OOP Basics/01. Defining Classes/Defining Classes - Exercise/CompanyRoster/CompanyRoster/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace CompanyRoster
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int n = int.Parse(Console.ReadLine());
+        List<Employee> employees = new List<Employee>();
+
+
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine("Hello World!");
+            string[] args = Console.ReadLine().Split();
+            string name = args[0];
+            decimal salary = decimal.Parse(args[1]);
+            string position = args[2];
+            string departement = args[3];
+            int age;
+            string email;
+
+            var employee = new Employee(name, salary, position, departement);
+
+            if (args.Length > 4)
+            {
+                if (int.TryParse(args[4], out age))
+                {
+                    employee.Age = int.Parse(args[4]);
+                }
+                else
+                {
+                    employee.Email = args[4];
+                }
+            }
+
+            if (args.Length > 5)
+            {
+                employee.Age = int.Parse(args[5]);
+            }
+
+            employees.Add(employee);
+        }
+
+        var department = employees
+            .GroupBy(em => em.Department)
+            .Select(gr => new
+            {
+                Name = gr.Key,
+                AvarangeSalary = gr.Average(em => em.Salary),
+                Employees = gr
+            })
+            .OrderByDescending(gr => gr.AvarangeSalary)
+            .FirstOrDefault();
+
+        Console.WriteLine($"Highest Average Salary: {department.Name}");
+        foreach (var emp in department.Employees.OrderByDescending(em=>em.Salary))
+        {
+            Console.WriteLine(emp.PrintEmployeeInfo());
         }
     }
 }
+
